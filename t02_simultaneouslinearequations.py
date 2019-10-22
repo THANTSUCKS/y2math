7d89916ede126c41e8e9cfc4de678f7bfafa66e3
THANTSUCKS <44459231+THANTSUCKS@users.noreply.github.com>
2019-10-22 14:21:45 +0800

    Update t02_simultaneouslinearequations.py

--- t02_simultaneouslinearequations.py
+++ t02_simultaneouslinearequations.py
@@ -1,5 +1,30 @@
 # Find x and y with these two equations:
 # 3x + 4y = 24
 # 4x + 3y = 22
-x=69
-y=69
+  
+#include <bits/stdc++.h> 
+using namespace std; 
+  
+// Function to find the 
+// values of X and Y 
+void findValues(int a, int b) 
+{ 
+    // base condition 
+    if ((a - b) % 2 == 1) { 
+        cout << "-1"; 
+        return; 
+    } 
+  
+    // required answer 
+    cout << (a - b) / 2 << " " << (a + b) / 2; 
+} 
+  
+// Driver Code 
+int main() 
+{ 
+    int a = 12, b = 8; 
+  
+    findValues(a, b); 
+  
+    return 0; 
+} 
